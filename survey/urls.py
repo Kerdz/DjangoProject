df8b688df56ee6ca392b3d619e49a3c9caa41bb8
Kerdeam Findley <86058276+Kerdz@users.noreply.github.com>
2025-05-03 14:48:06 -0400

    changes to urls

--- survey/urls.py
+++ survey/urls.py
@@ -4,8 +4,7 @@ from django.urls import include, path
 
 app_name = "survey"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("survey/", include("survey.urls")),
+    path("", views.index, name="survey_home"),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
